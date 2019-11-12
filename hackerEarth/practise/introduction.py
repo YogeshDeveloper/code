4699c49873988f0175cc1bbf6f1f935a3ed8cabd
YogeshDeveloper <spyogesh1994@gmail.com>
2019-11-12 23:54:50 +0530

    started learning new course on udemy

---
+++ hackerEarth/practise/introduction.py
@@ -0,0 +1,17 @@
+# the code is tutorial from HackerEarth
+ 
+# a = input("Enter the input")
+# print (a)
+
+# print('5'*6)
+# print(5,6,7)
+# print('LOVE',30,82.2)
+# print('LOVE',30,82.2,sep='.')
+# print('I am Yogesh\nand I can code')
+
+# for i in "python":
+#     print(i)
+
+for i in "python":
+    print(i,sep='_')
+  
